7387585a6fbe9e36cbfc8da722abc0b7daf6ef30
Cody-Joe Abe <cody.masao@gmail.com>
2019-02-06 15:28:01 -0800

    made colors nicesr

--- Processing/LissajousCurveTable/Lissajous/Lissajous.js
+++ Processing/LissajousCurveTable/Lissajous/Lissajous.js
@@ -2,7 +2,7 @@ let canvas;
 let LENGTH = 7;
 let gridScalar = 70;
 let DIAM = 50
-let baseX = 50;
+  let baseX = 50;
 let baseY = 50;
 let circlesX = [];
 let circlesY = [];
@@ -11,53 +11,51 @@ let dotsY = [];
 let dottedX = [];
 let dottedY = [];
 let xDots = [];
-let colors = ['#EEAAAA','#EEC5AA','#EEE0AA','#C5EEAA','#AAEEC5','#AAEEE0','#AAE0EE'];
+let colors = ['#EEAAAA', '#EEC5AA', '#EEE0AA', '#C5EEAA', '#AAEEC5', '#AAEEE0', '#AAE0EE'];
 function setup() {
-  canvas = createCanvas(500,500);
+  canvas = createCanvas(500, 500);
 
-  for(let i = 0 ; i < 7 ; i++){
-    circlesX.push(new Circle(baseX+gridScalar*(i+1), baseY, DIAM));
+  for (let i = 0; i < 7; i++) {
+    circlesX.push(new Circle(baseX+gridScalar*(i+1), baseY, DIAM, colors[i]));
   }
-  for(let i = 0 ; i < 7 ; i++){
-    circlesY.push(new Circle(baseX, baseY+gridScalar*(i+1), DIAM));
+  for (let i = 0; i < 7; i++) {
+    circlesY.push(new Circle(baseX, baseY+gridScalar*(i+1), DIAM, colors[i]));
   }
 
 
-  for(let i = 0 ; i < 7 ; i++){
+  for (let i = 0; i < 7; i++) {
     dotsX.push(new Dot(baseX+gridScalar*(i+1), baseY, DIAM));
   }
-   for(let i = 0 ; i < 7 ; i++){
+  for (let i = 0; i < 7; i++) {
     dotsY.push(new Dot(baseX, baseY+gridScalar*(i+1), DIAM));
   }
 
 
-  for(let i = 0 ; i < 7 ; i++){
+  for (let i = 0; i < 7; i++) {
     dottedX.push(new DottedLine(dotsX[i].getX(), dotsX[i].getY(), canvas.height));
   }
-   for(let i = 0 ; i < 7 ; i++){
+  for (let i = 0; i < 7; i++) {
     dottedY.push(new DottedLine(dotsY[i].getX(), dotsY[i].getY(), canvas.width));
   }
 
-  for(let i = 0 ; i < 7 ; i++){
+  for (let i = 0; i < 7; i++) {
     let row = [];
-    for(let j = 0 ; j < 7 ; j++){
-      row.push(new xDot(dotsX[i].getX(), dotsY[j].getY(), colors[j]));
+    for (let j = 0; j < 7; j++) {
+      row.push(new xDot(dotsX[i].getX(), dotsY[j].getY(), averageColor(colors[i],colors[j])));
     }
     xDots.push(row);
   }
-
-
 }
 
 
 function draw() {
-   background(255);
+  background(100);
 
-  for(let i = 0 ; i < LENGTH ; i++){
+  for (let i = 0; i < LENGTH; i++) {
     circlesX[i].display();
     circlesY[i].display();
   }
-  for(let i = 0 ; i < LENGTH ; i++){
+  for (let i = 0; i < LENGTH; i++) {
     dotsX[i].move((i+2)/2);
     dotsX[i].display();
 
@@ -70,107 +68,126 @@ function draw() {
     dottedY[i].move(dotsY[i].getX(), dotsY[i].getY());
     dottedY[i].displayY();
 
-    for(let j = 0 ; j < LENGTH ; j++){
+    for (let j = 0; j < LENGTH; j++) {
       xDots[i][j].display();
       xDots[i][j].move(dotsX[i].getX(), dotsY[j].getY());
     }
-
   }
-
-
-
-
-
 }
 
-class Circle{
-  constructor(x, y, d) {
+class Circle {
+  constructor(x, y, d, c) {
     this.x = x;
     this.y = y;
     this.d = d;
+    this.c = c;
   }
 
-  getPosition(){
-    return {x: this.x, y: this.y, diameter: this.d};
+  getPosition() {
+    return {
+    x:
+    this.x, y:
+    this.y, diameter:
+      this.d
+    };
   }
 
-  display(){
+  display() {
     strokeWeight(2);
+    stroke(color(this.c))
+      fill(100);
     ellipse(this.x, this.y, this.d, this.d);
   }
 }
 
-class Dot{
-  constructor(x,y,d){
+class Dot {
+  constructor(x, y, d) {
     this.angle = 0;
     this.x = x;
     this.y = y;
     this.d = d;
   }
-  getX(){
+  getX() {
     return this.x+(this.d/2) * cos(radians(this.angle));
   }
-  getY(){
+  getY() {
     return this.y+(this.d/2) * sin(radians(this.angle));
   }
 
-  display(){
+  display() {
     strokeWeight(6);
+    stroke(255);
     point((this.x+(this.d/2) * cos(radians(this.angle))), (this.y+(this.d/2) * sin(radians(this.angle))));
   }
 
-  move(a){
+  move(a) {
     this.angle += a;
   }
-
 }
 
-class xDot{
-  constructor(x,y,c){
+class xDot {
+  constructor(x, y, c) {
     this.x = x;
     this.y = y;
     this.c = c;
   }
 
-  display(){
-    strokeWeight(1);
-    fill(color(this.c))
-    point(this.x,this.y);
+  display() {
+    strokeWeight(4);
+    stroke(color(this.c))
+    point(this.x, this.y);
   }
-  move(x,y){
+  move(x, y) {
     this.x = x;
     this.y = y;
   }
 }
 
 class DottedLine {
-  constructor(p1,p2,d){
+  constructor(p1, p2, d) {
     this.angle = 0;
     this.p1 = p1;
     this.p2 = p2;
     this.d = d;
   }
 
-  displayX(){
+  displayX() {
     strokeWeight(1);
+    stroke(200)
     for (let i = 0; i <= 100; i++) {
       let x = lerp(this.p1, this.p1, i/100.0);
       let y = lerp(this.p2, this.d, i/100.0);
       point(x, y);
     }
   }
-  displayY(){
+  displayY() {
     strokeWeight(1);
+    stroke(200)
     for (let i = 0; i <= 100; i++) {
       let x = lerp(this.p1, this.d, i/100.0);
       let y = lerp(this.p2, this.p2, i/100.0);
       point(x, y);
     }
   }
-  move(p1,p2){
+  move(p1, p2) {
     this.p1 = p1;
     this.p2 = p2;
     point(this.p1, this.p2)
   }
+}
 
+function averageColor(color1,color2){
+    var avg  = function(a,b){ return (a+b)/2; },
+        t16  = function(c){ return parseInt((''+c).replace('#',''),16) },
+        hex  = function(c){ var t = (c>>0).toString(16);
+                           return t.length == 2 ? t : '0' + t },
+        hex1 = t16(color1),
+        hex2 = t16(color2),
+        r    = function(hex){ return hex >> 16 & 0xFF},
+        g    = function(hex){ return hex >> 8 & 0xFF},
+        b    = function(hex){ return hex & 0xFF},
+        res  = '#' + hex(avg(r(hex1),r(hex2)))
+                   + hex(avg(g(hex1),g(hex2)))
+                   + hex(avg(b(hex1),b(hex2)));
+    return res;
 }
